3947c70a6bd0dcdc482b639d9a705bf988ff2f02
William Giles <wdgstl@gmail.com>
2025-02-05 16:02:10 -0500

    model pipeline set up + data processing in place"

--- scripts/get_news.py
+++ scripts/get_news.py
@@ -19,21 +19,67 @@ rest_client = client.api
 
 #plot opening price 
 
-#Jan 4 to Feb 4 
+#Jan 6 to 31 
 
+#FETCH OPENING PRICES
+
+#MODIFY LOOP TO WORK FOR TIMEFRAME
+
+#USE OPENAI GPT
+
+dates = ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
+TIMEFRAME = "1D"
 
 news_agg = []
 def fetch_news(portfolio):
     for src in portfolio:
-        for n in range(1, 7):
-            news = rest_client.get_news(src, start=f'2025-01-0{n}T00:00:00Z', end=f'2022-01-0{n}T23:59:59Z', limit=50)
+        for n in dates:
+            news = rest_client.get_news(src, start=f'2025-01-{n}T00:00:00Z', end=f'2025-01-{n}T23:59:59Z', limit=50)
             news_agg.append(news)
     return news_agg
+
+prices_agg = []
+def fetch_prices(portfolio):
+    for src in portfolio:
+        for n in dates:
+            prices = rest_client.get_bars(src, TIMEFRAME, start=f'2025-01-{n}T00:00:00Z', end=f'2025-01-{n}T23:59:59Z')
+            prices_agg.append(prices)
+    return prices_agg
+
 
 
 
 if __name__ == "__main__":
     SYMBOL = ['NVDA']
-    news =  fetch_news(SYMBOL)
+    #GET NEWS DATA
+    news_agg =  fetch_news(SYMBOL)
+    news_data = []
     for n in news_agg:
-        print(len(n))
+        for news in n:
+            news_data.append({
+                "date": news.created_at,
+                "headline": news.headline,
+                "summary": news.summary
+            })
+
+    df_news = pd.DataFrame(news_data)
+
+    df_news.to_csv("news_data.csv", index=False)  
+
+    #GET PRICES DATA 
+    prices_agg = fetch_prices(SYMBOL)
+    prices_data = []
+    for p in prices_agg:
+        for price in p:
+            prices_data.append({
+                 "date": price.t,
+                 "price": price.o,
+             })
+    df_prices = pd.DataFrame(prices_data)
+
+    df_prices.to_csv("prices_data.csv", index=False)  
+
+         
+
+
+    
