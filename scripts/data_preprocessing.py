befae5f80a0591ccc767e205bd73d27309378872
Chai <zdc4tp@virginia.edu>
2025-02-12 20:54:14 -0500

    Refactored some of the codes, mainly get_news.py

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -17,7 +17,6 @@ def fix_prices_dates(path_to_csv):
     df.to_csv("price_data_processed.csv", index=False)  
 
 
-
 if __name__ == "__main__":
     aggregate_articles('/Users/wdgstl/UVA/DS/MarketSentimentAnalysis/data/news_data.csv')
     fix_prices_dates('/Users/wdgstl/UVA/DS/MarketSentimentAnalysis/data/prices_data.csv')
